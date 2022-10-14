defb4fe659c3abe6c3f10684be995debc88c7d6c
nenofi <nenofi@proton.me>
2022-10-15 02:26:04 +0700

    deploy needs fixing, bug most likely comes from emit events in for loop

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,17 +19,15 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   const NEIDR_ADDRESS = "0x5d6771aF6066619e42E364E734Cd2F5cCbBF8211";
 
-  let gamestarts = Math.floor(new Date('October 13, 2022 02:00:00 GMT+7').getTime() / 1000)
+  let gamestarts = Math.floor(new Date('October 16, 2022 20:00:00 GMT+7').getTime() / 1000)
+  console.log(gamestarts)
 
 
+  // const Bookmaker = await ethers.getContractFactory("BookmakerV01");
+  // var bookmaker = await Bookmaker.deploy(NEIDR_ADDRESS, gamestarts, "ManUtd-Newcastle");
+  // tx1.wait()
+  // console.log("bookmaker deployed to: " + bookmaker.address)
 
-  const Bookmaker = await ethers.getContractFactory("BookmakerV01");
-  var bookmaker = await Bookmaker.deploy(NEIDR_ADDRESS, gamestarts, "Milan-Chelsea");
-
-  console.log("bookmaker deployed to: " + bookmaker.address)
-  console.log(await bookmaker.admin())
-  // console.log(await bookmaker.eventName())
-  // console.log(await bookmaker.gameStarts())
 
   const provider = new ethers.providers.Web3Provider(network.provider);
   const managerWallet = new ethers.Wallet(process.env.MANAGER_KEY)
@@ -37,13 +35,24 @@ async function main() {
   console.log(managerAccount.address)
   
   const neIDR = new ethers.Contract(NEIDR_ADDRESS, neIDRabi.abi, managerAccount)
+  const bookmaker = new ethers.Contract('0x177f0dD8D4a605288a2570f4cdf83dEF23a4bcD3', bookmakerAbi.abi, managerAccount)
   await neIDR.connect(managerAccount).approve(bookmaker.address, ethers.constants.MaxUint256);
-  console.log(await neIDR.balanceOf(managerAccount.address))
-  await bookmaker.connect(managerAccount).bet(ethers.BigNumber.from("500000000000000000000000"), 0)
-  console.log(await neIDR.balanceOf(managerAccount.address))
-  // console.log("TRASNFERING")
-  // await neIDR.transfer('0x5d6771aF6066619e42E364E734Cd2F5cCbBF8211', ethers.BigNumber.from("500000000000000000000000"))
   // console.log(await neIDR.balanceOf(managerAccount.address))
+  var decimals = "000000000000000000"
+
+  var homeOdds = 500000/2.05
+  var home = homeOdds.toFixed(0).toString()+decimals
+  var drawOdds = 500000/3.94
+  var draw = drawOdds.toFixed(0).toString()+decimals
+  var awayOdds = 500000/3.87
+  var away = awayOdds.toFixed(0).toString()+decimals
+
+
+  console.log(home)
+  console.log(draw)
+  console.log(away)
+  tx3 = await bookmaker.connect(managerAccount).batchBet([ethers.BigNumber.from(home),ethers.BigNumber.from(draw),ethers.BigNumber.from(away)], {gasLimit: 500000, nonce: 44 })
+  tx3.wait()
 
 
 
