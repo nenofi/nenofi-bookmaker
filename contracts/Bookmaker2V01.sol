a9150f69e783d9a03220ccd1a4065d78fa548984
nenofi <nenofi@proton.me>
2022-10-26 11:51:49 +0700

    added betwithpermit on 2v01

--- contracts/Bookmaker2V01.sol
+++ contracts/Bookmaker2V01.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
+// Bookmaker2V01
+
+// ███╗░░██╗███████╗███╗░░██╗░█████╗░███████╗██╗
+// ████╗░██║██╔════╝████╗░██║██╔══██╗██╔════╝██║
+// ██╔██╗██║█████╗░░██╔██╗██║██║░░██║█████╗░░██║
+// ██║╚████║██╔══╝░░██║╚████║██║░░██║██╔══╝░░██║
+// ██║░╚███║███████╗██║░╚███║╚█████╔╝██║░░░░░██║
+// ╚═╝░░╚══╝╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝░░░░░╚═╝
+
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interface/IERC20Permit.sol";
 
 contract Bookmaker2V01 {
+    
     address public admin;
     address public betToken;
 
@@ -14,12 +24,13 @@ contract Bookmaker2V01 {
     uint256 public fee;
     uint256[2] public potPerResult;
     uint256 public gameStarts;
-    uint8 public winner; // 0 to bet for a win, 1 to bet for a draw, 2 to bet for a loss
+    uint8 public winner; 
 
     bool public claimable;
  
     event LogBet(address indexed better, uint256 amount, uint result);
     event LogClaim(address indexed claimer, uint256 amount);
+    event logBatchBet(address indexed better, uint256[] amount);
 
     modifier onlyAdmin(){
         require(msg.sender == admin, "BOOKMAKER: NOT ADMIN");
@@ -36,7 +47,7 @@ contract Bookmaker2V01 {
     function bet(address _betToken, uint256 _amount, uint8 _result) external{
         require(_betToken == betToken, "BOOKMAKER: YOUR TOKEN AREN'T ACCEPTED");
         require(block.timestamp < gameStarts, "BOOKMAKER: BETS ARE NOT ACCEPTED");
-        IERC20(_betToken).transferFrom(msg.sender, address(this), _amount);
+        IERC20Permit(_betToken).transferFrom(msg.sender, address(this), _amount);
         userBet[msg.sender][_result] += _amount;
         potPerResult[_result] += _amount;
         totalPot += _amount;
@@ -44,8 +55,36 @@ contract Bookmaker2V01 {
         emit LogBet(msg.sender, _amount, _result);
     }
 
+    function betWithPermit(uint256 _amount, uint256 _deadline, uint8 _result, uint8 v, bytes32 r, bytes32 s) external{
+        require(block.timestamp < gameStarts, "BOOKMAKER: BETS ARE NOT ACCEPTED");
+
+        IERC20Permit(betToken).permit(msg.sender, address(this), _amount, _deadline, v, r, s);
+        IERC20Permit(betToken).transferFrom(msg.sender, address(this), _amount);
+        userBet[msg.sender][_result] += _amount;
+        potPerResult[_result] += _amount;
+        totalPot += _amount;
+
+        emit LogBet(msg.sender, _amount, _result);
+
+    }
+
+    function batchBet(uint256[] memory _amount) public {
+        require(block.timestamp < gameStarts, "BOOKMAKER: BETS ARE NOT ACCEPTED");
+
+        uint256 thisAmount;
+        for(uint8 i=0; i < potPerResult.length; i++){
+            IERC20Permit(betToken).transferFrom(msg.sender, address(this), _amount[i]);
+            userBet[msg.sender][i] += _amount[i];
+            potPerResult[i] += _amount[i];
+            thisAmount += _amount[i];
+        }
+        totalPot += thisAmount;
+
+        emit logBatchBet(msg.sender, _amount);
+    }
+
     function getTotalPot() external view returns (uint256){
-        return IERC20(betToken).balanceOf(address(this));
+        return IERC20Permit(betToken).balanceOf(address(this));
     }
 
     function setWinner(uint8 _winner) external onlyAdmin{
@@ -65,7 +104,7 @@ contract Bookmaker2V01 {
         require(claimable == true, "BOOKMAKER: STATE IS NOT CLAIMABLE");
 
         uint256 userWinnings = losersPot * userBet[msg.sender][winner] / potPerResult[winner];
-        IERC20(betToken).transfer(msg.sender, userWinnings + userBet[msg.sender][winner]);
+        IERC20Permit(betToken).transfer(msg.sender, userWinnings + userBet[msg.sender][winner]);
         userBet[msg.sender][winner] = 0;
 
         emit LogClaim(msg.sender, userWinnings);
@@ -84,11 +123,11 @@ contract Bookmaker2V01 {
     }
 
     function claimFee() external onlyAdmin{
-        IERC20(betToken).transfer(admin, fee);
+        IERC20Permit(betToken).transfer(admin, fee);
     }
 
     function emergencyWithdraw(address _token, uint256 _amount) external onlyAdmin{
-        IERC20(_token).transfer(admin, _amount);
+        IERC20Permit(_token).transfer(admin, _amount);
     }
     
 }
